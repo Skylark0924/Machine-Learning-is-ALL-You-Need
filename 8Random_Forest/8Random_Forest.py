4ad64dd2c4e7ff02f3c3cd6caa8dd931df8a0072
Skylark <24936522+Skylark0924@users.noreply.github.com>
2020-04-06 21:55:44 +0800

    DT, RF sklearn done

---
+++ 8Random_Forest/8Random_Forest.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
+if __name__ == '__main__':
+    use_sklearn = True
+
+    # Data Preprocessing
+    dataset = pd.read_csv('./dataset/Social_Network_Ads.csv')
+    X = dataset.iloc[:, [2, 3]].values
+    Y = dataset.iloc[:, 4].values
+
+    # Making Dataset
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.25, random_state=0)
+
+    # Feature Scaling
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train.astype(np.float64))
+    X_test = sc.transform(X_test.astype(np.float64))
+
+    if use_sklearn:
+        from sklearn.ensemble import RandomForestClassifier
+        classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+        classifier.fit(X_train, Y_train)
+    else:
+        classifier = Skylark_RandomForestClassifier(
+            k_neighbors=5
+        )
+
+    Y_pred = classifier.predict(X_test)
+
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(Y_test, Y_pred)
+    print('Random Forest:\n', cm)
+
+    # Visualising the Training set results
+    from matplotlib.colors import ListedColormap
+    X_set, y_set = X_train, Y_train
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                        np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title('Random Forest Classification (Training set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+
+    # Visualising the Test set results
+    from matplotlib.colors import ListedColormap
+    X_set, y_set = X_test, Y_test
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                        np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label = j)
+    plt.title('Random Forest Classification (Test set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
